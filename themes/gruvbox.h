b19a69a29a48817c8308568175b55e1cd373aed0
Skerbergs <freddy.skerbergs@protonmail.com>
2024-01-31 23:49:44 +0100

    nord theme + comments

--- themes/gruvbox.h
+++ themes/gruvbox.h
@@ -1,6 +1,6 @@
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#cc241d";
-static const char col_gray3[]       = "#ebdbb2";
-static const char col_gray4[]       = "#282828";
-static const char col_cyan[]        = "#d79921";
+static const char col_gray1[]       = "#282828"; // Title bar background
+static const char col_gray2[]       = "#cc241d"; // Inactive window border
+static const char col_gray3[]       = "#ebdbb2"; // Inactive tag foreground
+static const char col_gray4[]       = "#282828"; // Active tag foreground
+static const char col_cyan[]        = "#d79921"; // Active window border and tag background
 
---
+++ themes/nord.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#2e3440"; // Title bar background
+static const char col_gray2[]       = "#4c566a"; // Inactive window border
+static const char col_gray3[]       = "#d8dee9"; // Inactive tag foreground
+static const char col_gray4[]       = "#2e3440"; // Active tag foreground
+static const char col_cyan[]        = "#81a1c1"; // Active window border and tag background
+
