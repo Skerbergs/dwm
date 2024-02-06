a3812b44df08946537b4a4e7f4213cb05a397ae4
Skerbergs <freddy.skerbergs@protonmail.com>
2024-02-06 16:02:09 +0100

    FreeBSD theme

---
+++ themes/freebsd.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#282828"; // Title bar background
+static const char col_gray2[]       = "#cc241d"; // Inactive window border
+static const char col_gray3[]       = "#fe8019"; // Inactive tag foreground
+static const char col_gray4[]       = "#282828"; // Active tag foreground
+static const char col_cyan[]        = "#fb4934"; // Active window border and tag background
+
