f9899edabffa7199ccb7eb990fcfef76be6bb0a3
Skerbergs <freddy.skerbergs@protonmail.com>
2024-02-15 14:34:07 +0100

    Void Linux theme

---
+++ themes/void.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#222222"; // Title bar background
+static const char col_gray2[]       = "#478061"; // Inactive window border
+static const char col_gray3[]       = "#819181"; // Inactive tag foreground
+static const char col_gray4[]       = "#222222"; // Active tag foreground
+static const char col_cyan[]        = "#62b086"; // Active window border and tag background
+
